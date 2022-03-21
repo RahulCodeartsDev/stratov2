83af65017910195f5e1f28b8b6844cd4319b9446
RahulCodeartsDev <rahul.codearts@gmail.com>
2022-03-21 10:34:12 +0530

    Updated

--- src/app/material-module/material-module.module.ts
+++ src/app/material-module/material-module.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 
 const material = [
   MatBadgeModule,
-  MatButtonModule
+  MatButtonModule,
+  MatInputModule
 ]
 
 
